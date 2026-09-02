ec5e44f085bfc41d03877b932a37b0df15420998
yeimyrojas24401-cyber <yeimy.rojas.24401@gmail.com>
2026-09-02 01:35:58 -0600

    mainMenuUpdates

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,10 +1,9 @@
-using UnityEngine;
 using TMPro;
+// si hay algo gris no lo necesito
+using UnityEngine;
 using UnityEngine.UI;
-using System;
-using Unity.VisualScripting;
 
-public class MainMenu : MonoBehaviour
+public class mainMenu : MonoBehaviour
 {
     [Header("Buttons")]
     [SerializeField] private Button btnPlay;
@@ -13,49 +12,40 @@ public class MainMenu : MonoBehaviour
     [SerializeField] private Button btnExit;
 
     [Header("Panels")]
-
     [SerializeField] private GameObject mainMenuCanvas;
     [SerializeField] private GameObject settingsPanel;
     [SerializeField] private GameObject creditsPanel;
 
-    [Header("Sliders")]
-
+    [Header("Slider")]
     [SerializeField] private Slider sliderPlayer1Speed;
     [SerializeField] private Slider sliderPlayer2Speed;
 
     [Header("Players")]
-
     [SerializeField] private Movement player1;
     [SerializeField] private Movement player2;
 
-    [Header("TextPlayers")]
+    [Header("SpeedPlayersTMP")]
     [SerializeField] private TMP_Text textSpeedPlayer1;
     [SerializeField] private TMP_Text textSpeedPlayer2;
 
-
-
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     private void Awake()
     {
-       // Buttons
-        btnPlay.onClick.AddListener(OnPlayClicked);
+        btnPlay.onClick.AddListener(OnPlayClicked); // "When click in btnPlay ejecuta OnContinueClicked
         btnSettings.onClick.AddListener(OnSettingsClicked);
         btnCredits.onClick.AddListener(OnCreditsClicked);
         btnExit.onClick.AddListener(OnExitClicked);
 
-        sliderPlayer1Speed.onValueChanged.AddListener(OnPlayer1SpeedChanged);
+        sliderPlayer1Speed.onValueChanged.AddListener(OnPlayer1SpeedChanged); //cuando el valor del slider cambie ejecuta OnPlayer1SpeedChanged
         sliderPlayer2Speed.onValueChanged.AddListener(OnPlayer2SpeedChanged);
     }
-
-
-    private void Start()
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
     {
-        //Panels
         settingsPanel.SetActive(false);
         creditsPanel.SetActive(false);
     }
-    private void OnDestroy()
+
+    private void OnDestroy() // for each add Listener we need to put one remove listener es como decir cuando este objeto vaya a destruirse, ya no ejecutes las cosas que estaban conectadas conmigo
     {
         btnPlay.onClick.RemoveListener(OnPlayClicked);
         btnSettings.onClick.RemoveListener(OnSettingsClicked);
@@ -65,41 +55,42 @@ public class MainMenu : MonoBehaviour
         sliderPlayer1Speed.onValueChanged.RemoveListener(OnPlayer1SpeedChanged);
         sliderPlayer2Speed.onValueChanged.RemoveListener(OnPlayer2SpeedChanged);
     }
-    private void OnPlayClicked()
+
+    private void OnPlayClicked() // this is the function called OnContinueClicked which is going to run when the btnPlay click
     {
-        mainMenuCanvas.SetActive(false);
+        mainMenuCanvas.SetActive(false); //  When Play is clicked, this GameObject becomes inactive.
     }
 
     private void OnSettingsClicked()
     {
-        settingsPanel.SetActive(true);
-        creditsPanel.SetActive(false);
-        mainMenuCanvas.SetActive(false);
-
         sliderPlayer1Speed.value = player1.moveSpeed;
         sliderPlayer2Speed.value = player2.moveSpeed;
+        mainMenuCanvas.SetActive(false);
+        settingsPanel.SetActive(true);
+        creditsPanel.SetActive(false);
     }
+
     private void OnCreditsClicked()
     {
         creditsPanel.SetActive(true);
-        settingsPanel.SetActive(false);
         mainMenuCanvas.SetActive(false);
-
+        settingsPanel.SetActive(false);
     }
 
-    private void OnPlayer1SpeedChanged (float value) // DENEMOS PONER float value porque asi le estamos diciendo que cuando sea llamada esta funcion va a recibir un float value
+    private void OnPlayer1SpeedChanged(float value) // esta funcion va a recibir un numero decimal cuando sea llamada
     {
         player1.moveSpeed = value;
-        textSpeedPlayer1.text = value.ToString("F1"); // El F1 nos permite que se pongan valores sin decimal
+        // float percentage = value * 25f; //esto nos permite hacer una equivalencia para que la velocidad se ponga en %
+        textSpeedPlayer1.text = value.ToString("F1");
     }
+
     private void OnPlayer2SpeedChanged(float value)
     {
         player2.moveSpeed = value;
-        textSpeedPlayer2.text = value.ToString("F1"); // El F1 nos permite que se pongan valores sin decimal
+        textSpeedPlayer2.text = value.ToString("F1");
     }
     private void OnExitClicked()
     {
         UnityEditor.EditorApplication.isPlaying = false;
     }
-
 }
