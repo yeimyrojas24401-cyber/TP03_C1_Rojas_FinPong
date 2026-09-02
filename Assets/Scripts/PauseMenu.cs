6808c80237c5a1eb9c590f7c1c669b6bef5c8d8e
yeimyrojas24401-cyber <yeimy.rojas.24401@gmail.com>
2026-09-02 01:34:48 -0600

    pauseMenuUpdates

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -3,21 +3,19 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PauseMenu : MonoBehaviour
+public class pauseMenu : MonoBehaviour
 {
-    [Header("Buttons")]
     [SerializeField] private Button btnContinue;
     [SerializeField] private Button btnSettings;
     [SerializeField] private Button btnCredits;
     [SerializeField] private Button btnExit;
 
-    [Header("Panels")]
 
-    [SerializeField] private GameObject settingsPanel;
     [SerializeField] private GameObject creditsPanel;
+    [SerializeField] private GameObject settingsPanel;
     [SerializeField] private GameObject pausePanel;
 
-    [Header("Sliders")]
+    [Header("Slider")]
     [SerializeField] private Slider sliderPlayer1Speed;
     [SerializeField] private Slider sliderPlayer2Speed;
 
@@ -25,28 +23,26 @@ public class PauseMenu : MonoBehaviour
     [SerializeField] private Movement player1;
     [SerializeField] private Movement player2;
 
-    [Header("Players")]
-    [SerializeField] private TMP_Text TextPlayer1;
-    [SerializeField] private TMP_Text TextPlayer2;
-   
+    [Header("SpeedPlayersTMP")]
+    [SerializeField] private TMP_Text textSpeedPlayer1;
+    [SerializeField] private TMP_Text textSpeedPlayer2;
+
     private bool isPause = false;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    private void Awake ()
+    private void Awake() // solo para add listener y get components porque esta es la inicializacion (todas las referencias)
     {
-        btnContinue.onClick.AddListener(OnContinueClicked);
+        btnContinue.onClick.AddListener(OnContinueClicked); // "When click in btnPlay ejecuta OnContinueClicked
         btnSettings.onClick.AddListener(OnSettingsClicked);
         btnCredits.onClick.AddListener(OnCreditsClicked);
         btnExit.onClick.AddListener(OnExitClicked);
 
-        sliderPlayer1Speed.onValueChanged.AddListener(OnPlayer1SpeedChanged);
+        sliderPlayer1Speed.onValueChanged.AddListener(OnPlayer1SpeedChanged); //cuando el valor del slider cambie ejecuta OnPlayer1SpeedChanged
         sliderPlayer2Speed.onValueChanged.AddListener(OnPlayer2SpeedChanged);
     }
 
-
     private void Start()
     {
-        creditsPanel.SetActive(false);
+        creditsPanel.SetActive(false); // alt + flecha hacia abajo y arriba para mover lineas 
         settingsPanel.SetActive(false);
     }
 
@@ -58,51 +54,57 @@ public class PauseMenu : MonoBehaviour
         }
     }
 
-    private void OnDestroy()
+    private void OnDestroy() // for each add Listener we need to put one remove listener es como decir cuando este objeto vaya a destruirse, ya no ejecutes las cosas que estaban conectadas conmigo
     {
-        btnContinue.onClick.RemoveListener(OnContinueClicked);
-        btnSettings.onClick.RemoveListener(OnSettingsClicked);
-        btnCredits.onClick.RemoveListener(OnCreditsClicked);
-        btnExit.onClick.RemoveListener(OnExitClicked);
+        btnContinue.onClick.RemoveAllListeners();
+        btnSettings.onClick.RemoveAllListeners();
+        btnCredits.onClick.RemoveAllListeners();
+        btnExit.onClick.RemoveAllListeners();
 
         sliderPlayer1Speed.onValueChanged.RemoveListener(OnPlayer1SpeedChanged);
         sliderPlayer2Speed.onValueChanged.RemoveListener(OnPlayer2SpeedChanged);
     }
+
     private void OnContinueClicked()
     {
-            isPause = !isPause;
-            pausePanel.SetActive(isPause);
-            if (isPause)
-            {
-                Time.timeScale = 0f;
-            }
-            else
-            {
-                Time.timeScale = 1f;
-            }
+        isPause = !isPause; //!igual a lo opuesto
+        pausePanel.SetActive(isPause);
+        if (isPause)
+        {
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            Time.timeScale = 1f;
+        }
     }
+
     private void OnSettingsClicked()
     {
         sliderPlayer1Speed.value = player1.moveSpeed;
         sliderPlayer2Speed.value = player2.moveSpeed;
         settingsPanel.SetActive(true);
     }
+
     private void OnCreditsClicked()
     {
         creditsPanel.SetActive(true);
     }
+
     private void OnExitClicked()
     {
         UnityEditor.EditorApplication.isPlaying = false;
     }
-    private void OnPlayer1SpeedChanged(float value)
-    {
-        player1.moveSpeed = value;
-        TextPlayer1.text = value.ToString("F1");
-    }
+
     private void OnPlayer2SpeedChanged(float value)
     {
         player2.moveSpeed = value;
-        TextPlayer2.text = value.ToString("F1");
+        textSpeedPlayer2.text = value.ToString("F1");
+    }
+
+    private void OnPlayer1SpeedChanged(float value)
+    {
+        player1.moveSpeed = value;
+        textSpeedPlayer1.text = value.ToString("F1");
     }
 }
