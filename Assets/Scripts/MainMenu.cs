729c9fed1f449f35f2c2d48a670b4a0150015330
yeimyrojas24401-cyber <yeimy.rojas.24401@gmail.com>
2026-09-01 22:46:58 -0600

    MainMenuScriptCreation
    empece el script para mainmenu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,73 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System;
+
+public class MainMenu : MonoBehaviour
+{
+    [Header("Buttons")]
+    [SerializeField] private Button btnPlay;
+    [SerializeField] private Button btnSettings;
+
+    [SerializeField] private Button btnCredits;
+    [SerializeField] private Button btnExit;
+
+    [Header("Panels")]
+
+    [SerializeField] private GameObject mainMenuCanvas;
+    [SerializeField] private GameObject settingsPanel;
+    [SerializeField] private GameObject creditsPanel;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Awake()
+    {
+       // Buttons
+        btnPlay.onClick.AddListener(OnPlayClicked);
+        btnSettings.onClick.AddListener(OnSettingsClicked);
+        btnCredits.onClick.AddListener(OnCreditsClicked);
+        btnExit.onClick.AddListener(OnExitClicked);
+    }
+
+    private void Start()
+    {
+        //Panels
+        mainMenuCanvas.SetActive(true);
+        settingsPanel.SetActive(false);
+        creditsPanel.SetActive(false);
+    }
+    private void OnDestroy()
+    {
+        btnPlay.onClick.RemoveListener(OnPlayClicked);
+        btnSettings.onClick.RemoveListener(OnSettingsClicked);
+        btnCredits.onClick.RemoveListener(OnCreditsClicked);
+        btnExit.onClick.RemoveListener(OnExitClicked);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnPlayClicked()
+    {
+        mainMenuCanvas.SetActive(false);
+    }
+
+    private void OnSettingsClicked()
+    {
+        settingsPanel.SetActive(true);
+        creditsPanel.SetActive(false);
+        mainMenuCanvas.SetActive(false);
+    }
+    private void OnCreditsClicked()
+    {
+        creditsPanel.SetActive(true);
+        settingsPanel.SetActive(false);
+        mainMenuCanvas.SetActive(false);
+
+    }
+    private void OnExitClicked()
+    {
+        throw new NotImplementedException();
+    }
+
+}
